148eb4703b21a914457d886305727439b0da77bf
Astridxx <muratkozan350@gmail.com>
2023-11-14 16:49:33 +0300

    haydn: DerpFest-14-Ify

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,29 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
+# Device common
+echo 'Cloning common device tree'
+git clone https://github.com/xiaomi-haydn-devs/android_device_xiaomi_sm8350-common -b Derpfest-14 device/xiaomi/sm8350-common
+
+# Kernel
+echo 'Cloning kernel tree'
+git clone https://github.com/xiaomi-haydn-devs/android_kernel_xiaomi_sm8350.git --recursive --depth=1 -b lineage-21 kernel/xiaomi/sm8350
+
+# Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/xiaomi-haydn-devs/proprietary_vendor_xiaomi_haydn -b lineage-21 vendor/xiaomi/haydn
+
+# Vendor common
+echo 'Cloning common vendor tree'
+git clone https://github.com/xiaomi-haydn-devs/proprietary_vendor_xiaomi_sm8350-common -b lineage-21 vendor/xiaomi/sm8350-common
+
+# Xiaomi
+echo 'Cloning hardware xiaomi'
+rm -rf hardware/xiaomi && git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+
+# Kernel tools
+echo 'Cloning prebuilts kernel-build-tools'
+rm -rf prebuilts/kernel-build-tools && git clone --depth=1 https://github.com/PixelExperience-Staging/prebuilts_kernel-build-tools -b fourteen prebuilts/kernel-build-tools
+
 # Firmware
 echo 'Cloning firmware'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
