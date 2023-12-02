936f68a846d1ade45802dd8df43345eb840c5bef
Murat Kozan <muratkozan350@gmail.com>
2023-12-02 12:56:55 +0300

    haydn: Add optimization patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,4 +22,19 @@ wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/VoNR
 patch -p1 <0001-Enable-VoNR-by-default.patch
 cd ../..
 
+echo 'Adding optimization patch'
+cd system/core
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/Optimization/0001-Dont-enable-f2fs-iostat-by-default.patch
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/Optimization/0002-Tune-F2FS-and-dirty-writeout-policy-for-modern-devices.patch
+patch -p1 <0001-Dont-enable-f2fs-iostat-by-default.patch
+patch -p1 <0002-Tune-F2FS-and-dirty-writeout-policy-for-modern-devices.patch
+cd ../..
+
+cd frameworks/base
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/Optimization/0003-BootReceiver-Return-early-if-trace-pipe-doesnt-exists.patch
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/Optimization/0004-Remove-USB-charging-notification.patch
+patch -p1 <0003-BootReceiver-Return-early-if-trace-pipe-doesnt-exists.patch
+patch -p1 <0004-Remove-USB-charging-notification.patch
+cd ../..
+
 echo 'delete vendorsetup.sh from device tree once this is done'
