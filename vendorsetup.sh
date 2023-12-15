140ce7c5e20f2d89ce0ae800cacee0cf66a9549c
Astridxx <muratkozan350@gmail.com>
2023-12-15 19:01:20 +0300

    haydn: Add VoNR patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,4 +9,10 @@ echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera -b fourteen-leica vendor/xiaomi/haydn-miuicamera
 rm -rf hardware/xiaomi/megvii
 
+echo 'Enabling VoNR by default'
+cd frameworks/base
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/udc-14/VoNR/0001-Enable-VoNR-by-default.patch
+patch -p1 <0001-Enable-VoNR-by-default.patch
+cd ../..
+
 echo 'delete vendorsetup.sh from device tree once this is done'
