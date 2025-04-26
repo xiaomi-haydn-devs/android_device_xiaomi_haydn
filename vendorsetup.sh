772572daec52e2fdc1289439c8614209c435c8c8
Alucard Stormbringer <supersayan1000@gmail.com>
2025-04-26 12:58:05 +0530

    haydn: vendorsetup: update leica patch
    add back the backwards compatible constructor for resolution fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,9 +27,9 @@ echo 'Cloning Signing keys'
 git clone --depth=1 https://github.com/xiaomi-haydn-devs/priv_keys.git vendor/xiaomi/priv-keys
 
 # Leica Patch
-cd frameworks/native
-wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/refs/heads/14/Leicamera/0001-Add-back-redundant-connect-methods-in-Surface.patch
-patch -p1 <0001-Add-back-redundant-connect-methods-in-Surface.patch
+cd frameworks/base
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
+patch -p1 <0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
 cd ../..
 
 # nuke fsgen
